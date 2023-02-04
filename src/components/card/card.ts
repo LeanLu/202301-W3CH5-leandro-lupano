0c7397fdf206fd0797de3a0ad4eadcfeeda3dc32
Leandro <leandrolupano@hotmail.com>
2023-02-04 19:53:06 +0100

    💄 Add card style

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import { PokemonStructure } from '../../models/pokemonStructure';
 import { Component } from '../component/component';
+import './card.scss';
 
 export class Card extends Component {
   constructor(public selector: string, public pokemon: PokemonStructure) {
@@ -20,8 +21,8 @@ export class Card extends Component {
         <ul class="pokemons-grid">
           <li class="card">
             <img class="pokemon-picture" src="${this.pokemon.picture}" alt="${this.pokemon.name}">
-            <span class="pokemon-name">Name: ${this.pokemon.name}</span>
-            <button class="button-add-favorite">⭐Add to favorites ➕</button>
+            <span class="pokemon-name">${this.pokemon.name}</span>
+            <button class="button-add-favorite">⭐Add to favorites</button>
           </li>
         </ul>
       </section>
