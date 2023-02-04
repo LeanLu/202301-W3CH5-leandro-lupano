35c26738d6a7831061264819ae192e8a094f433f
Leandro <leandrolupano@hotmail.com>
2023-02-04 18:37:21 +0100

    👷 Add basic card structure

---
+++ src/components/card/card.ts
@@ -0,0 +1,30 @@
+/* eslint-disable no-unused-vars */
+import { PokemonStructure } from '../../models/pokemonStructure';
+import { Component } from '../component/component';
+
+export class Card extends Component {
+  constructor(public selector: string, public pokemon: PokemonStructure) {
+    super();
+    this.pokemon = { ...pokemon };
+    this.template = this.createTemplate();
+    this.render('afterbegin');
+  }
+
+  render(place: globalThis.InsertPosition) {
+    super.render(place);
+  }
+
+  createTemplate() {
+    return `
+      <section class="pokemons">
+        <ul class="pokemons-grid">
+          <li class="card">
+            <img class="pokemon-picture" src="${this.pokemon.picture}" alt="${this.pokemon.name}">
+            <span class="pokemon-name">Name: ${this.pokemon.name}</span>
+            <button class="button-add-favorite">⭐Add to favorites ➕</button>
+          </li>
+        </ul>
+      </section>
+    `;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,19 @@
 /* eslint-disable no-new */
+import { Card } from './components/card/card';
 import { Header } from './components/header/header';
 import { Menu } from './components/menu/menu';
 import './index.scss';
+import { Pokemon } from './models/pokemonStructure';
 
 new Header('.app');
 
 new Menu('.header');
+
+const pikachu = new Pokemon(
+  'Pikachu',
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+  50,
+  100
+);
+
+new Card('.main', pikachu);
